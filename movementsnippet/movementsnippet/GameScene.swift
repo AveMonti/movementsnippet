d076838ca9a16e368a20249f8261299ac9085368
Mateusz Chojnacki <mateuszchojnacki@MacBook-Air-Mateusz.local>
2017-12-21 21:22:33 +0100

    moving

--- movementsnippet/movementsnippet/GameScene.swift
+++ movementsnippet/movementsnippet/GameScene.swift
@@ -10,80 +10,94 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+
+    var player:SKSpriteNode?
+    var movingMode = false
+    let moveSound = SKAction.playSoundFileNamed("Sounds/move.wav", waitForCompletion: false)
+    func createPlayer(){
+        player = SKSpriteNode(imageNamed: "player")
+        player?.position =  CGPoint(x:self.frame.width / 2,y : self.frame.height / 2)
+        self.addChild(player!)
+    }
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+    func moveVertically(up: Bool){
+        if up{
+            let moveAction = SKAction.moveBy(x: 0, y:  3, duration: 0.01)
+            let repeatAction = SKAction.repeatForever(moveAction)
+            player?.run(repeatAction)
+        }else{
+            let moveAction = SKAction.moveBy(x: 0, y:  -3, duration: 0.01)
+            let repeatAction = SKAction.repeatForever(moveAction)
+            player?.run(repeatAction)
         }
+    }
+    
+    func moveHoryzontalny(side:Bool){
+        player?.removeAllActions()
+        movingMode = true
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        if let player = self.player{
+            let verdicalSide:CGPoint?
+            if side{ //right
+                verdicalSide = CGPoint(x:player.position.x + 40 , y:player.position.y)
+            }else{ // left
+                verdicalSide = CGPoint(x:player.position.x - 40 , y:player.position.y)
+            }
+            let moveAction = SKAction.move(to: verdicalSide!, duration: 0.4)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            player.run(moveAction, completion: {
+                self.movingMode = false
+            })
+            
+            self.run(moveSound)
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        if let tauch = touches.first{
+            let location = tauch.previousLocation(in: self)
+            let node = self.nodes(at: location).first
+            if node?.name == "right"{
+                moveHoryzontalny(side: true)
+            }else if node?.name == "left"{
+                moveHoryzontalny(side: false)
+            }else if node?.name == "up"{
+                print("move Up")
+                moveVertically(up: true)
+                
+            } else if node?.name == "down"{
+                print("move Down")
+                moveVertically(up: false)
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        if !movingMode{
+            player?.removeAllActions()
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        player?.removeAllActions()
     }
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    override func didMove(to view: SKView) {
+        createPlayer()
+    }
+    
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
     }
 }
